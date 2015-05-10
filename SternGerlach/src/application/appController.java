93a83c76b5f1da1b6f1180a064c538c12c9929e6
Kabat <kabathlock@gmail.com>
2015-05-11 00:05:28 +0200

    Znikanie magnesów

--- SternGerlach/src/application/appController.java
+++ SternGerlach/src/application/appController.java
@@ -338,7 +338,6 @@ public class appController implements Initializable{
         Rotate rotation = new Rotate(dy/180);
         rotation.setAxis(Rotate.Y_AXIS);
         
-        if ((y<340 && y>280)&&(x<265 && x>210)){
           	 secondMagnet.getTransforms().add(rotation);
           	 theta2 -= dy/180;
           	 if (theta2>360)
@@ -347,7 +346,6 @@ public class appController implements Initializable{
          		 theta2+=360;
           	 ang2.setText(Integer.toString((int)theta2) + " " + ResourceBundle.getBundle("application.lang.lang",lang).getString("deg"));
            }
-        if ((y<300 && y>220)&&(x<380 && x>320)){
           	 thirdMagnet.getTransforms().add(rotation);
           	 theta3 -=dy/180;
           	 if (theta3>360)
